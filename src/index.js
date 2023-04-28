3d4734065ece04c6d8b710fa516261f1a1364601
Antifishka <101981622+Antifishka@users.noreply.github.com>
2023-04-28 17:41:54 +0300

    change BrowserRouter to HashRouter, change homepage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { theme } from 'globalStyles/theme';
 import App from 'App';
@@ -8,11 +8,11 @@ import App from 'App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/tweets-test">
+    <HashRouter>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
